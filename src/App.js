01368137208267ef7a9e884da8607d8f67374598
Becky449 <rabnab12@gmail.com>
2023-02-09 17:09:43 +0300

    Created full metrics web app using COVID-19 API

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Details from './components/Display';
+import Heading from './components/Header';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Heading />
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:updated" element={<Details />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
